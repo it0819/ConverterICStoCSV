0b98c3313db83c6ea1496c2355b7ceb814be2adb
Bartosz <63149844+MrCodetti@users.noreply.github.com>
2020-06-11 13:46:19 +0200

    Uptade mit Timestamp Funktion

--- writeLogfile.cpp
+++ writeLogfile.cpp
@@ -1,3 +1,13 @@
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%m-%d_%X", &tstruct);
+    return buf;
+}
+
+
 void writeLogfile(string notiz, string icsName) {
 	string logName = icsName + "_logfile.log";
 	
